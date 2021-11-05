33efffa9caeeb46b0658fd0392f29aa9d7f8ab60
Janis Kalofolias <kalofoli@ceid.upatras.gr>
2021-11-05 03:19:09 +0100

    PersistenParallel

--- colito/parallel.py
+++ colito/parallel.py
@@ -33,4 +33,49 @@ try:
             self._pbar.n = self.n_completed_tasks
             self._pbar.refresh()
 
+    class PersistentProgressParallel(joblib.Parallel):
+        def __init__(self, *args, tqdm_opts={}, total=None, progress=True, **kwargs):
+            super().__init__(*args, **kwargs)
+            self._progress = progress
+            self._tqdm_opts = {'total':total, **tqdm_opts}
+            self._tqdm = None
+            self.__entered = False
+            self.__n_completed_tasks = 0
+        @property
+        def progressbar(self): return self._tqdm
+        @property
+        def progress(self): return self._progress
+        @progress.setter
+        def progress(self, value):
+            if not value and self._progress:
+                self._progress_stop()
+            if value and self.__entered:
+                self._progress_start()
+            self._progress = value
+        def __call__(self, *args, **kwargs):
+            if hasattr(self, 'n_completed_tasks'):
+                self.__n_completed_tasks += self.n_completed_tasks
+            return super().__call__(*args, **kwargs)
+        def _progress_stop(self):
+            if self._tqdm is not None:
+                self._tqdm.__exit__(None,None,None)
+                self._tqdm = None
+        def _progress_start(self):
+            if self._progress:
+                self._tqdm = tqdm(**self._tqdm_opts).__enter__()
+                self._tqdm.n = self.__n_completed_tasks
+                    
+        def print_progress(self):
+            if self._tqdm is not None:
+                self._tqdm.n = self.__n_completed_tasks + self.n_completed_tasks
+                self._tqdm.refresh()
+        def __enter__(self):
+            self.__entered = False
+            self._progress_start()
+            return super().__enter__()
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            self._progress_stop()
+            self.__entered = False
+            return super().__exit__(exc_type, exc_val, exc_tb)
+
 except ImportError: pass
